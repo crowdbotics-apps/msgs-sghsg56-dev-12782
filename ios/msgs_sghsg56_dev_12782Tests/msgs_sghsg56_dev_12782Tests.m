10f95c9e897dcad50e21950879adc97b9fe689f4
Crowdbotics <team@crowdbotics.com>
2020-10-06 09:11:48 +0000

    Initial commit.

---
+++ ios/msgs_sghsg56_dev_12782Tests/msgs_sghsg56_dev_12782Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface msgs_sghsg56_dev_12782 : XCTestCase
+
+@end
+
+@implementation msgs_sghsg56_dev_12782
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
